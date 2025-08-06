1948100054a3627440f589c6fdfe8a6e3e4333f5
Codiemunos-01 <codiemunos@gmail.com>
2025-08-05 23:57:13 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
 #GoogleCollab is the platform that will be used to create this project.
 #You must run the cells from top to bottom in order for all libraries to be installed for the code to run properly.
 #A kaggle account is needed in order to retrieve the datasets needed and get an API key.
+#Upload the Kaggle dataset to google collab's 'file' tab
